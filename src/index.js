174653db523d541fab80da39e1da71444804ef95
Simon <aguilarsimon01@gmail.com>
2020-07-08 23:39:37 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+
+// midelwares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+// routes
+app.use(require('./routes/index'));
+app.use(require('./routes/users'));
+
+// starting the server
+app.listen(app.get('port'), () => {
+      console.log(`Server on port ${app.get('port')}`);
+});
+
+const { Router } = require('express');
+const router = Router();
+
+router.get('/test', (req, res) => {
+      const data = {
+            "name": "Simonito",
+            "surname":"Fazt"
+      };
+      res.json(data);
+});
+
+
+module.exports = router;
